5b22aa41c275a52bac4427a8c8baccb1997e7ae7
magswag <magnus.o.randby@gmail.com>
2019-02-08 12:17:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let x = 0;
+let y = 0;
+
 function setup() {
 createCanvas(windowWidth, windowHeight);
 }
@@ -6,7 +9,7 @@ function draw() {
 background(255);
 fill(0);
 noStroke();
-  let x = lerp(x, mouseX, 0.1);
-  let y = lerp(y, mouseY, 0.1);
+  x = lerp(x, mouseX, 0.1);
+  y = lerp(y, mouseY, 0.1);
 ellipse(x, y, 40, 40);
 }
