1ec6a722b8a27880afbcdfc976117712ca4fb6bc
magswag <magnus.o.randby@gmail.com>
2019-02-08 12:14:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,5 +6,7 @@ function draw() {
 background(255);
 fill(0);
 noStroke();
-ellipse(mouseX, mouseY, 40, 40);
+  let x = lerp(x, mouseX, 0.1);
+  let y = lerp(y, mouseY, 0.1);
+ellipse(x, y, 40, 40);
 }
